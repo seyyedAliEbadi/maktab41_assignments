a1b5340fd4b51c69dd07ae1e9669410e3000ea7b
Seyyed Ali Ebadi <ebadiseyyedali@gmail.com>
2020-11-10 21:29:42 +0330

    Add deleteByIndex and combineArr function

--- front_hw3/index.js
+++ front_hw3/index.js
@@ -1,1 +1,22 @@
 // Delete item by index in an array
+function deleteByIndex(arr, index) {
+    console.log(arr);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    console.log(arr)
+}
+
+// Combining two or more arrays
+function combineArr() {
+    let concatArr = [];
+    for (item of arguments) {
+        console.log(item)
+        concatArr = concatArr.concat(item);
+    }
+
+    console.log(concatArr)
+}
+let myArr = ['a', 'b', 'c'];
+deleteByIndex(myArr, 2)
+combineArr([1, 2, 3], [4, 5, 6, 7], [8, 9, 10])
