14ab4a390a9dc430dea1bbb8c1f402ca552a8c8c
Seyyed Ali Ebadi <ebadiseyyedali@gmail.com>
2020-11-10 22:55:56 +0330

    Add repItem

--- front_hw3/index.js
+++ front_hw3/index.js
@@ -30,6 +30,13 @@ function findByItem(arr, temp) {
     }
 }
 
+// Replace item in an array
+function repItem(arr, currentItem, newItem) {
+    let copiedArr = arr.map((x) => x);
+    let index = copiedArr.indexOf(currentItem);
+    copiedArr[index] = newItem;
+    return copiedArr
+}
 
 let myArr = ['a', 'b', 'c'];
 deleteByIndex(myArr, 2)
@@ -41,3 +48,6 @@ console.log(result)
 
 let flattened_arr = flatArr([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]);
 console.log(flattened_arr)
+
+let arr_copy = repItem([1, 2, 3, 4, 5], 3, 10)
+console.log(arr_copy)
