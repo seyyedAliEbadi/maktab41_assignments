130222471df679f3f1d1eb648edf339abe74ccea
Seyyed Ali Ebadi <ebadiseyyedali@gmail.com>
2020-11-10 22:45:15 +0330

    Add flatArr and findByItem

--- front_hw3/index.js
+++ front_hw3/index.js
@@ -17,6 +17,27 @@ function combineArr() {
 
     console.log(concatArr)
 }
+
+// Flatten infinite number of arrays
+function flatArr(arr) {
+    return arr.flat(Infinity)
+}
+
+// Find item in an array
+function findByItem(arr, temp) {
+    if (arr.indexOf(temp) != -1) {
+        return { index: arr.indexOf(temp), item: temp }
+    }
+}
+
+
 let myArr = ['a', 'b', 'c'];
 deleteByIndex(myArr, 2)
+
 combineArr([1, 2, 3], [4, 5, 6, 7], [8, 9, 10])
+
+let result = findByItem(myArr, 'b');
+console.log(result)
+
+let flattened_arr = flatArr([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]);
+console.log(flattened_arr)
